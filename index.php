5bc79e14febd1cccca77fc002ccd6176342d6c19
slifty <slifty@gmail.com>
2013-02-20 20:26:51 -0500

    added tracking

--- index.php
+++ index.php
@@ -101,6 +101,19 @@
 <link rel="canonical" href="http://www.theonion.com">
 <link rel="alternate" type="application/rss+xml" title="The Onion: Daily News" href="http://feeds.theonion.com/theonion/daily">
 <link rel="icon" type="image/png" href="favicon.ico">
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-36889397-4']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 </head>
 <body id="" class="site_onion ga_section afns-ad-skin" name="default" data-ct_section_name="blank" style="overflow: visible;">
 <section class="offpage" data-ct_section_name="offpage">
